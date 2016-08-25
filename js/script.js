b6761534470516a943906c35f77387b9039e7bee
samwisegrangee <Samwise@Samuels-MBP.netgear.com>
2016-08-25 17:48:44 -0400

    Modals & Lightboxes
    A lot was done today on getting modal windows to work for the Music
    page (which will be populated with links to his songs).
    
    The Posters use a mixture of my modal solution and lightbox solution at
    the moment. I want to fix the widths and the closing abilities tomorrow.
    
    Now for a run…

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 $( document ).ready(function() {
 
-//your code for stuff should go here
-  $('#lightBox').css('height', $(document).outerWidth() + 'px');
-  //for when you click on an image
+//Lightbox Display with Clicked Image
+  $('#lightBox').css('height', $(document).outerWidth() + 'px');//for when you click on an image
   $('.imgSm').click(function () {
       var src = $(this).attr('src'); //get the source attribute of the clicked image
       $('#lightBox img').attr('src', src); //assign it to the tag for your fullscreen div
       $('#lightBox').fadeIn();
   });
   $('#lightBox').click(function () {
-    $(this).fadeOut(); //this will hide the fullscreen div if you
+    $(this).fadeOut(); //this will hide the fullscreen div if you click it
   });
+  //$('.modal').click(function(){
+  //  $(this).fadeOut();
+//  })
+
+  //Toggle visibility of Song Players by Id
+  function toggleDiv(divId) {
+    $("#"+divId).toggle(1000);
+  };
 });
