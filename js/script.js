1d5740e5daaf848aeeab656f25609f97a551cd71
samwisegrangee <Samwise@Samuels-MBP.netgear.com>
2016-09-02 17:04:46 -0400

    Captions and Covers
    Today I did a lot of tinkering to find an efficient way to write
    captions to all of Glenn’s images. A few lines of JQ take the `title`
    and drop it into a `p` with the id of caption.
    
    Additionally, albums now have covers on the modal dropdown.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 $( document ).ready(function() {
 
 //Lightbox Display with Clicked Image
-  $('#lightBox').css('height', $(document).outerWidth() + 'px');//for when you click on an image
+  $('#lightBox').css('height', $(document).outerWidth() + 'px');//for when an image is clicked
   $('.imgSm').click(function () {
       var src = $(this).attr('src'); //get the source attribute of the clicked image
-      $('#lightBox img').attr('src', src); //assign it to the tag for your fullscreen div
+      var title=$(this).attr('title'); //get the title attribut of clicked image
+      $('#lightBox img').attr('src', src); //assign src to the fullscreen div tag
+      $('#lightBox img').attr('title', title); //assign title to the fullscreen div tag
+      $('#img-capt').html(title);
       $('#lightBox').fadeIn();
   });
+
+//  $('#img-capt').each(function() {
+//   $(this).after( "<div class='caption'><p>" + title + "</p></div>" );})
+
   $('#lightBox').click(function () {
     $(this).fadeOut(); //this will hide the fullscreen div if you click it
   });
+});
+
+
   //$('.modal').click(function(){
   //  $(this).fadeOut();
 //  })
 
-  //Toggle visibility of Song Players by Id
-  function toggleDiv(divId) {
-    $("#"+divId).toggle(1000);
-  };
-});
+//Toggle visibility of Song Players by Id
+function toggleDiv(divId) {
+  $("#"+divId).toggle(1000);
+};
