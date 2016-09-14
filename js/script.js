d6b9499158011a61428a2bebdbf6d5ed9e3bf051
samwisegrangee <Samwise@Samuels-MBP.netgear.com>
2016-09-14 15:56:37 -0400

    Copy, Imgs, Audio Controls
    Updated some events.
    Changed some images.
    Now only one audio track can play at a time.
    
    I still need to fix the bio button and spruce up the homepage.

--- js/script.js
+++ js/script.js
@@ -25,3 +25,12 @@ $( document ).ready(function() {
 function toggleDiv(divId) {
   $("#"+divId).toggle(1000);
 };
+
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
