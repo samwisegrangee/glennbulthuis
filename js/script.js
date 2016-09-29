63ae28688146e880bd9516aa3be8a3211e74722e
samwisegrangee <Samwise@Samuels-MBP.netgear.com>
2016-09-28 21:01:25 -0400

    Finishing Strong
    Major wrap-up: redid modal windows to drop down and scroll up the page
    by id. A lot of operations happen now with a single click.
    
    Added a footer for my rights and credit.
    
    Initial scal-1 -> Initial scale-1

--- js/script.js
+++ js/script.js
@@ -16,11 +16,6 @@ $( document ).ready(function() {
   });
 });
 
-
-  //$('.modal').click(function(){
-  //  $(this).fadeOut();
-//  })
-
 //Toggle visibility of Song Players by Id
 function toggleDiv(divId) {
   $("#"+divId).toggle(1000);
@@ -35,7 +30,28 @@ document.addEventListener('play', function(e){
     }
 }, true);
 
-//$('.album_listen').click(function(){
-//    $("html, body").animate({ scrollTop: 0 }, 600);
-//    return false;
-// });
+//Modal Drop Downs by Id: Make sure the <a>'s all have id="[modal tag]-open"
+$(function() {
+  $("[id$=-open]").click(function() {
+    var id = this.id;
+    var targetId = id.replace('-open', '');
+    var $target = $('#' + targetId);
+    $($target).css("display", "block");
+    $($target).css("overflow-y", "auto");
+    $("body").css("overflow", "hidden");
+    $(".modal-container").css("display", "block");
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow"); //
+  });
+});
+
+$(function() {
+  $(".close").click(function(e) {
+    e.preventDefault();
+    $(".modal").css("display", "none");
+    $(".modal").css("overflow-y", "");
+    $("body").css("overflow", "");
+    window.location.hash = $("#trigger").attr("id"); //
+  });
+});
